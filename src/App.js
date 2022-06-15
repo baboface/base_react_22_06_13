058e69eb7fa4a4b6289dc1ceaa1b45e9cde16a6b
baboface <baboface@gmail.com>
2022-06-15 09:57:18 +0900

    props

--- src/App.js
+++ src/App.js
@@ -18,7 +18,18 @@ const App = () => {
       <FnComponent_2 /> */}
       {/* <ClassComponent /> */}
 
-      <FoodMenu />
+      {/* *props
+      =><컴포넌트 프롭스명 = "값"/> */}
+
+      <h3>아침메뉴</h3>
+      <FoodMenu menuName="계란밥" sideMenu="마운틴듀" />
+
+      <h3>점심메뉴</h3>
+      <FoodMenu menuName="카레" sideMenu="제로콜라" />
+
+      <h3>저녁메뉴</h3>
+      <FoodMenu menuName="해장국" sideMenu="사이다" />
+      {/* 2일차 */}
     </div>
   );
 };
