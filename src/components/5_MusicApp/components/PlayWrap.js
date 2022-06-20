44f4c188c1e7546ae1f93d37b9568b6450c88f4c
baboface <baboface@gmail.com>
2022-06-20 10:39:29 +0900

    playwrap

--- src/components/5_MusicApp/components/PlayWrap.js
+++ src/components/5_MusicApp/components/PlayWrap.js
@@ -1,10 +1,29 @@
 import styled from "styled-components";
 
-const SPlayWrap = styled.div``;
+const SPlayWrap = styled.div`
+  width: 100%;
+  margin-top: 30px;
+`;
 
-const GrageWrap = styled.div``;
+const GrageWrap = styled.div`
+  width: 100%;
+  height: 5px;
+  background-color: gray;
+`;
 
-const Gage = styled.div``;
+const Gage = styled.div`
+  width: 10%;
+  height: 100%;
+  background-color: white;
+`;
+
+const TimeWrap = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 5px;
+  font-size: 12px;
+  font-weight: 100;
+`;
 
 export const PlayWrap = () => {
   return (
@@ -12,8 +31,10 @@ export const PlayWrap = () => {
       <GrageWrap>
         <Gage />
       </GrageWrap>
-      <span>00:03</span>
-      <span>03:40</span>
+      <TimeWrap>
+        <span>00:03</span>
+        <span>03:40</span>
+      </TimeWrap>
     </SPlayWrap>
   );
 };
